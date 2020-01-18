fcf0e574269d71791430e93468bb227260d59c96
Mateusz Sutor <msutor@student.agh.edu.pl>
2020-01-18 11:25:10 +0100

    New manage nuclear rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -4,5 +4,10 @@ public class SomeProgram {
 	public static void main(String[] args) { 
 		System.out.println("We are learning to use Git."); 
 		System.out.println("We will be Git experts, soon.");
-	} 
+	}
+	public void manageNuclearRockets() {
+    // TODO implement
 }
+	
+}
+
